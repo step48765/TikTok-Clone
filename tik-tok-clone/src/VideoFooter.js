4e03c45408dbee4316aa869859e64e08f27853bb
Michael Grant Warshowksy <mwarshowsky71@gmail.com>
2020-10-26 15:26:28 -0400

    work on footer
    made scpinning disck, music ticker, and description

--- tik-tok-clone/src/VideoFooter.js
+++ tik-tok-clone/src/VideoFooter.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./VideoFooter.css";
+import MusicNoteIcon from '@material-ui/icons/MusicNote'
+import Ticker from "react-ticker";
 
 function VideoFooter() {
   return (
@@ -7,6 +9,16 @@ function VideoFooter() {
       <div className="videoFooter__text">
         <h3>@rafehqzi</h3>
         <p>description</p>
+        <div className="videoFooter__ticker">
+          <MusicNoteIcon className="videoFooter__icon"/>
+          <Ticker mode="smooth">
+            {({ index }) => (
+              <>
+                <p>yo what suphj</p>
+              </>
+            )}
+          </Ticker>
+        </div>
       </div>
       <img className="videoFooter__record"
        src="https://static.thenounproject.com/png/934821-200.png" alt="">
